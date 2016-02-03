c8381b1a72d779f6159731c4f552ae3522bf8d81
Makoto Kitamura <today.is.sailingday24@gmail.com>
2016-02-03 22:02:57 +0900

    add post form

--- client/task-twitter-clone.js
+++ client/task-twitter-clone.js
@@ -1,15 +1,11 @@
 // counter starts at 0
 Session.setDefault('counter', 0);
 
-Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
+Template.postForm.helpers({
 });
 
-Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+Template.postForm.events({
+    'submit .new-post': function (event, template) {
+        alert(template.find('input[type="text"]').value);
     }
 });
